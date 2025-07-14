df2f06865f2fae2e1423cf045f0c121388d431ce
BOURZGUIHIBA <157542276+BOURZGUIHIBA@users.noreply.github.com>
2025-07-14 15:04:42 +0100

    exercice1.sh

---
+++ exercice1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Créer les répertoires
+mkdir -p camera photos
+
+# Créer les fichiers image dans le dossier camera
+touch camera/pic1.png
+touch camera/pic2.png
+touch camera/pic3.png
+touch camera/pic4.png
+
+# Créer les fichiers vidéo dans le dossier camera
+touch camera/vid1.mp4
+touch camera/vid2.mp4
+touch camera/vid3.mp4
+touch camera/vid4.mp4
