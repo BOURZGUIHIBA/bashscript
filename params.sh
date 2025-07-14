662d5cc150220468237279d87c789b3a84de33e4
BOURZGUIHIBA <157542276+BOURZGUIHIBA@users.noreply.github.com>
2025-07-14 14:26:26 +0100

    params.sh

---
+++ params.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Argument 1 : $1"
+echo "Argument 2 : $2"
+echo "Argument 3 : $3"
+echo "Argument 4 : $4"
+echo "Argument 5 : $5"
+echo "Argument 6 : $6"
+echo "Argument 7 : $7"
+echo "Argument 8 : $8"
+echo "Argument 9 : $9"
